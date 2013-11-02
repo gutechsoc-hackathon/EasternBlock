dfc47ba1cf5d4707481e9ade8570eb5e5da24903
Raziel Alphadios <raziely@gmail.com>
2013-11-02 17:10:40 +0000

    Fragments, tabs, adapters

--- app/gen/com/guts/hackathon/R.java
+++ app/gen/com/guts/hackathon/R.java
@@ -21,22 +21,31 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int event=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int listicon=0x7f020002;
-        public static final int mapicon=0x7f020003;
-        public static final int question=0x7f020004;
+        public static final int anchor=0x7f020000;
+        public static final int event=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int listicon=0x7f020003;
+        public static final int mapicon=0x7f020004;
+        public static final int question=0x7f020005;
     }
     public static final class id {
         public static final int actBarButtonMap=0x7f080000;
-        public static final int actBarButtonMap2=0x7f080001;
-        public static final int actBarButtonMap3=0x7f080002;
-        public static final int actBarButtonMap4=0x7f080003;
-        public static final int action_settings=0x7f080004;
+        public static final int actBarButtonSearch=0x7f080007;
+        public static final int eventsList=0x7f080004;
+        public static final int fragment_placeholder=0x7f080001;
+        public static final int listWrapperE=0x7f080003;
+        public static final int listWrapperQ=0x7f080005;
+        public static final int menu_hero_order=0x7f080008;
+        public static final int questionsList=0x7f080006;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int actionbar_c=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int event=0x7f030002;
+        public static final int events_list=0x7f030003;
+        public static final int question=0x7f030004;
+        public static final int questions_list=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- app/src/com/guts/hackathon/MainActivity.java
+++ app/src/com/guts/hackathon/MainActivity.java
@@ -2,25 +2,52 @@ package com.guts.hackathon;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.View;
+
 
 @SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
+	private DisplayMetrics metrics;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		metrics = this.getResources().getDisplayMetrics();
 		ActionBar aBar = getActionBar();
+		aBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		aBar.setDisplayShowHomeEnabled(false);
 		aBar.setDisplayShowTitleEnabled(false);
 		aBar.setDisplayUseLogoEnabled(false);
+
+	    Tab eventsTab = aBar.newTab()
+	                       .setText("E")
+	                       //.setIcon(R.drawable.listicon)
+	                       .setTabListener(new TabListener<EventsFragment>(
+		                           this, "events", EventsFragment.class));
+	    
+
+	    Tab questionsTab = aBar.newTab()
+	                   .setText("Q")
+	                   .setTabListener(new TabListener<QuestionsFragment>(
+	                           this, "questions", QuestionsFragment.class));
+	    
+	    aBar.addTab(eventsTab);
+	    aBar.addTab(questionsTab);
+
 		//displaying custom ActionBar
 		View mActionBarView = getLayoutInflater().inflate(R.layout.actionbar_c, null);
 		aBar.setCustomView(mActionBarView);
 		aBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		//Set actionbar button positioning
+
 		setContentView(R.layout.activity_main);
 	}
 
@@ -30,5 +57,68 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		
 		return true;
+	}
+	
+/*    @SuppressLint("NewApi")
+    private class PositioningLC implements OnLayoutChangeListener {
+
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right,
+                            int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                	Log.d("LayoutTest","left="+left+", top="+top+", right="+right+", bottom="+bottom);
+                    if (left == 0 && top == 0 && right == 0 && bottom == 0) {
+                            return;
+                    }
+                    ImageView leftmostButton = (ImageView) findViewById(R.id.actBarButtonMap);
+                    ImageView events = (ImageView) findViewById(R.id.actBarButtonEvent);
+                    ImageView questions = (ImageView) findViewById(R.id.actBarButtonQuestion);
+                    RelativeLayout.LayoutParams lParams = (LayoutParams) leftmostButton.getLayoutParams();
+                    lParams.rightMargin = (metrics.widthPixels/2) - ((events.getWidth()+questions.getWidth())/2);
+                    leftmostButton.setLayoutParams(lParams);
+            }
+
+    } */
+	
+	public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+	    private Fragment mFragment;
+	    private final Activity mActivity;
+	    private final String mTag;
+	    private final Class<T> mClass;
+
+	    /** Constructor used each time a new tab is created.
+	      * @param activity  The host Activity, used to instantiate the fragment
+	      * @param tag  The identifier tag for the fragment
+	      * @param clz  The fragment's Class, used to instantiate the fragment
+	      */
+	    public TabListener(Activity activity, String tag, Class<T> clz) {
+	        mActivity = activity;
+	        mTag = tag;
+	        mClass = clz;
+	    }
+
+	    /* The following are each of the ActionBar.TabListener callbacks */
+
+	    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+	        // Check if the fragment is already initialized
+	        if (mFragment == null) {
+	            // If not, instantiate and add it to the activity
+	            mFragment = Fragment.instantiate(mActivity, mClass.getName());
+	            ft.add(android.R.id.content, mFragment, mTag);
+	        } else {
+	            // If it exists, simply attach it in order to show it
+	            ft.attach(mFragment);
+	        }
+	    }
+
+	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	        if (mFragment != null) {
+	            // Detach the fragment, because another one is being attached
+	            ft.detach(mFragment);
+	        }
+	    }
+
+	    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	        // User selected the already selected tab. Usually do nothing.
+	    }
 	}
 }
